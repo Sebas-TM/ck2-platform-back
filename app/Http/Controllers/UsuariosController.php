188c3fceb9504188a5209a98df4c2c4bcdb4cab3
comunik2admin <comunik2admin@ck2.com>
2023-05-22 21:04:31 -0500

    first commit

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Usuarios;
+
+class UsuariosController extends Controller
+{
+    //
+    function createUsuarios(Request $request){
+        $nombre = $request->get('nombre');
+        $apellido_paterno = $request->get('apellido_paterno');
+        $apellido_materno = $request->get('apellido_materno');
+        $username = $request->get('username');
+        $password = $request->get('password');
+        $isAdmin = $request->get('isAdmin');
+
+        $createdUsuario = Usuarios::create([
+            'nombre'=>$nombre,
+            'apellido_paterno'=>$apellido_paterno,
+            'apellido_materno'=>$apellido_materno,
+            'username'=>$username,
+            'password'=>$password,
+            'isAdmin'=>$isAdmin
+        ]);
+
+        return response($createdUsuario, 201);
+
+        function listUsuarios(Request $request){
+            $listedUsuarios = Usuarios::all();
+            return response($listedUsuarios,200);
+        }
+        
+        // 'nombre',
+        // 'apellido_paterno',
+        // 'apellido_materno',
+        // 'username',
+        // 'password',
+        // 'permiso'
+    }
+}
---
+++ app/Models/Usuarios.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuarios extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = [
+        'nombre',
+        'apellido_paterno',
+        'apellido_materno',
+        'username',
+        'password',
+        'isAdmin'
+    ];
+}
